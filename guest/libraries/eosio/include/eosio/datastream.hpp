d5e6b57200dd35a17698d44915e68924045b5672
Vladimir Tarnakin <v.tarnakin@everypixel.ru>
2026-07-20 12:10:20 +0500

    fix: constrain eosio datastream fallback [skip ci]

--- guest/libraries/eosio/include/eosio/datastream.hpp
+++ guest/libraries/eosio/include/eosio/datastream.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <boost/pfr/core.hpp>
+#include <concepts>
 #include <cstddef>
 #include <list>
 #include <map>
@@ -33,21 +34,61 @@ template <typename Storage> class datastream : public forge::contract::datastrea
    datastream(base_type stream) : base_type(std::move(stream)) {}
 };
 
-template <typename Storage, typename T> datastream<Storage>& operator<<(datastream<Storage>& stream, const T& value);
-template <typename Storage, typename T> datastream<Storage>& operator>>(datastream<Storage>& stream, T& value);
+namespace detail {
+
+template <typename T>
+inline constexpr bool character_pointer_v =
+    std::is_pointer_v<std::remove_cv_t<T>> &&
+    std::same_as<std::remove_cv_t<std::remove_pointer_t<std::remove_cv_t<T>>>, char>;
+
+template <typename Stream, typename T>
+inline constexpr bool writable_v =
+    std::is_arithmetic_v<std::remove_cv_t<T>> || std::is_enum_v<std::remove_cv_t<T>> ||
+    std::same_as<std::remove_cv_t<T>, std::byte> || character_pointer_v<T> || std::is_array_v<T> ||
+    ::forge::raw::detail::built_in_codec_v<T> || ::forge::raw::detail::adl_packable<Stream, T> ||
+    ::forge::raw::detail::traits_packable<Stream, T> || (std::is_class_v<T> && std::is_aggregate_v<T>);
+
+template <typename Stream, typename T>
+inline constexpr bool readable_v =
+    std::is_arithmetic_v<std::remove_cv_t<T>> || std::is_enum_v<std::remove_cv_t<T>> ||
+    std::same_as<std::remove_cv_t<T>, std::byte> || std::is_array_v<T> || ::forge::raw::detail::built_in_codec_v<T> ||
+    ::forge::raw::detail::adl_unpackable<Stream, T> || ::forge::raw::detail::traits_unpackable<Stream, T> ||
+    (std::is_class_v<T> && std::is_aggregate_v<T>);
+
+} // namespace detail
+
+template <typename Storage, typename T>
+   requires(detail::writable_v<datastream<Storage>, T>)
+datastream<Storage>& operator<<(datastream<Storage>& stream, const T& value);
+
+template <typename Storage, typename T>
+   requires(detail::readable_v<datastream<Storage>, T>)
+datastream<Storage>& operator>>(datastream<Storage>& stream, T& value);
 
 template <typename Storage, typename Enable, typename T>
-::forge::datastream<Storage, Enable>& operator<<(::forge::datastream<Storage, Enable>& stream, const T& value);
+   requires(detail::writable_v<::forge::datastream<Storage, Enable>, T>)::forge::datastream<Storage, Enable>
+& operator<<(::forge::datastream<Storage, Enable>& stream, const T& value);
 
 template <typename Storage, typename Enable, typename T>
-::forge::datastream<Storage, Enable>& operator>>(::forge::datastream<Storage, Enable>& stream, T& value);
+   requires(detail::readable_v<::forge::datastream<Storage, Enable>, T>)::forge::datastream<Storage, Enable>
+& operator>>(::forge::datastream<Storage, Enable>& stream, T& value);
 
 namespace detail {
 
 template <typename Stream, typename T> Stream& write(Stream& stream, const T& value) {
-   if constexpr (std::is_class_v<T> && std::is_aggregate_v<T> && !::forge::raw::detail::adl_packable<Stream, T> &&
-                 !::forge::raw::detail::built_in_codec_v<T>) {
+   if constexpr (std::is_array_v<T>) {
+      ::forge::raw::pack(stream, ::forge::unsigned_int{std::extent_v<T>});
+      for (const auto& item : value) {
+         stream << item;
+      }
+   } else if constexpr (std::is_class_v<T> && std::is_aggregate_v<T> &&
+                        !::forge::raw::detail::adl_packable<Stream, T> &&
+                        !::forge::raw::detail::traits_packable<Stream, T> &&
+                        !::forge::raw::detail::built_in_codec_v<T>) {
       boost::pfr::for_each_field(value, [&](const auto& field) { stream << field; });
+   } else if constexpr (::forge::raw::detail::traits_packable<Stream, T> &&
+                        !::forge::raw::detail::adl_packable<Stream, T>) {
+      ::forge::raw::codec_traits<std::remove_cv_t<T>>::pack(stream, value);
    } else {
       ::forge::raw::pack(stream, value);
    }
@@ -55,9 +96,21 @@ template <typename Stream, typename T> Stream& write(Stream& stream, const T& va
 }
 
 template <typename Stream, typename T> Stream& read(Stream& stream, T& value) {
-   if constexpr (std::is_class_v<T> && std::is_aggregate_v<T> && !::forge::raw::detail::adl_unpackable<Stream, T> &&
-                 !::forge::raw::detail::built_in_codec_v<T>) {
+   if constexpr (std::is_array_v<T>) {
+      auto size = ::forge::unsigned_int{};
+      ::forge::raw::unpack(stream, size);
+      ::forge::raw::detail::require(size.value == std::extent_v<T>, "T[] size and unpacked size don't match");
+      for (auto& item : value) {
+         stream >> item;
+      }
+   } else if constexpr (std::is_class_v<T> && std::is_aggregate_v<T> &&
+                        !::forge::raw::detail::adl_unpackable<Stream, T> &&
+                        !::forge::raw::detail::traits_unpackable<Stream, T> &&
+                        !::forge::raw::detail::built_in_codec_v<T>) {
       boost::pfr::for_each_field(value, [&](auto& field) { stream >> field; });
+   } else if constexpr (::forge::raw::detail::traits_unpackable<Stream, T> &&
+                        !::forge::raw::detail::adl_unpackable<Stream, T>) {
+      ::forge::raw::codec_traits<std::remove_cv_t<T>>::unpack(stream, value);
    } else {
       ::forge::raw::unpack(stream, value);
    }
@@ -66,21 +119,27 @@ template <typename Stream, typename T> Stream& read(Stream& stream, T& value) {
 
 } // namespace detail
 
-template <typename Storage, typename T> datastream<Storage>& operator<<(datastream<Storage>& stream, const T& value) {
+template <typename Storage, typename T>
+   requires(detail::writable_v<datastream<Storage>, T>)
+datastream<Storage>& operator<<(datastream<Storage>& stream, const T& value) {
    return detail::write(stream, value);
 }
 
-template <typename Storage, typename T> datastream<Storage>& operator>>(datastream<Storage>& stream, T& value) {
+template <typename Storage, typename T>
+   requires(detail::readable_v<datastream<Storage>, T>)
+datastream<Storage>& operator>>(datastream<Storage>& stream, T& value) {
    return detail::read(stream, value);
 }
 
 template <typename Storage, typename Enable, typename T>
-::forge::datastream<Storage, Enable>& operator<<(::forge::datastream<Storage, Enable>& stream, const T& value) {
+   requires(detail::writable_v<::forge::datastream<Storage, Enable>, T>)::forge::datastream<Storage, Enable>
+& operator<<(::forge::datastream<Storage, Enable>& stream, const T& value) {
    return detail::write(stream, value);
 }
 
 template <typename Storage, typename Enable, typename T>
-::forge::datastream<Storage, Enable>& operator>>(::forge::datastream<Storage, Enable>& stream, T& value) {
+   requires(detail::readable_v<::forge::datastream<Storage, Enable>, T>)::forge::datastream<Storage, Enable>
+& operator>>(::forge::datastream<Storage, Enable>& stream, T& value) {
    return detail::read(stream, value);
 }
 
--- guest/tests/consumer/eosio_datastream_adl.cpp
+++ guest/tests/consumer/eosio_datastream_adl.cpp
@@ -6,13 +6,35 @@ struct eosio_datastream_record {
    std::uint32_t value = 0;
 };
 
+struct eosio_datastream_array_record {
+   std::uint32_t values[2]{};
+};
+
+class eosio_datastream_unsupported {
+ public:
+   eosio_datastream_unsupported() {}
+};
+
+template <typename T>
+concept eosio_stream_writable = requires(eosio::datastream<char*>& stream, const T& value) { stream << value; };
+
+static_assert(eosio_stream_writable<eosio_datastream_record>);
+static_assert(eosio_stream_writable<eosio_datastream_array_record>);
+static_assert(!eosio_stream_writable<eosio_datastream_unsupported>);
+
 void compile_eosio_datastream_adl() {
-   char bytes[16]{};
+   char bytes[32]{};
    auto stream = eosio::datastream<char*>{bytes, sizeof(bytes)};
    auto input = eosio_datastream_record{42};
    stream << input;
 
+   auto array_input = eosio_datastream_array_record{{7, 9}};
+   stream << array_input;
+
    stream.seekp(0);
    auto output = eosio_datastream_record{};
    stream >> output;
+
+   auto array_output = eosio_datastream_array_record{};
+   stream >> array_output;
 }
